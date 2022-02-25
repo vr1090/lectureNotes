c3a0ff6c8625eea55385a3cc6aa3389ffe8498fb
inu <vr1090@gmail.com>
2022-02-25 20:29:25 +0700

    update lecture notes

---
+++ german vocab training.mm
@@ -0,0 +1,9 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1645776795386" ID="ID_612289722" MODIFIED="1645776804707" TEXT="german vocab training">
+<node CREATED="1645776804959" ID="ID_137395613" MODIFIED="1645776812469" POSITION="right" TEXT="unter.sche.id.en">
+<node CREATED="1645776812792" ID="ID_1869791915" MODIFIED="1645776817791" TEXT="diffrerentiate"/>
+<node CREATED="1645776818924" ID="ID_1592668855" MODIFIED="1645776821584" TEXT="perbedaan"/>
+</node>
+</node>
+</map>
