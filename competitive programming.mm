52aa9c6d5c57cf3b529c8ee8d3d361cc58c4d4e9
inu <vr1090@gmail.com>
2022-02-28 16:44:07 +0700

    update lecture notes

---
+++ competitive programming.mm
@@ -0,0 +1,11 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1646040817660" ID="ID_1566415613" MODIFIED="1646040829106" TEXT="competitive programming">
+<node CREATED="1646040829377" ID="ID_839731922" MODIFIED="1646040831524" POSITION="right" TEXT="graph">
+<node CREATED="1646040831781" ID="ID_493688574" MODIFIED="1646040833045" TEXT="tree">
+<node CREATED="1646040833327" ID="ID_1104164520" MODIFIED="1646040841278" TEXT="dfs(node, prev)"/>
+<node CREATED="1646040844826" ID="ID_451241589" MODIFIED="1646040854493" TEXT="if visited == number node"/>
+</node>
+</node>
+</node>
+</map>
