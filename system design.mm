ea22ef5c48105508f4433fd47c767fb0ca583cac
inu <vr1090@gmail.com>
2022-03-08 23:21:25 +0700

    update lecture notes

---
+++ system design.mm
@@ -0,0 +1,4 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1646756467298" ID="ID_191072173" MODIFIED="1646756471742" TEXT="system design"/>
+</map>
