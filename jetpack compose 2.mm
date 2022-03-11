88b0d078165a51ae12825c6c908e9be321acd2f6
inu <vr1090@gmail.com>
2022-03-11 19:07:17 +0700

    update lecture notes

---
+++ jetpack compose 2.mm
@@ -0,0 +1,9 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1646866247222" ID="ID_343791186" MODIFIED="1646866256621" TEXT="jetpack compose 2">
+<node CREATED="1646866257164" ID="ID_939187325" MODIFIED="1646866259607" POSITION="right" TEXT="scaffold">
+<node CREATED="1646866259877" ID="ID_381990005" MODIFIED="1646866264257" TEXT="topAppbar"/>
+<node CREATED="1646866264498" ID="ID_1684491907" MODIFIED="1646866264498" TEXT=""/>
+</node>
+</node>
+</map>
