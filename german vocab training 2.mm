c719e11245542cc842a6c57c280a8a569c71cb52
inu <vr1090@gmail.com>
2022-03-14 09:10:47 +0700

    update lecture notes

---
+++ german vocab training 2.mm
@@ -0,0 +1,11 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1647139367409" ID="ID_142540211" MODIFIED="1647139374858" TEXT="german vocab training 2">
+<node CREATED="1647139375113" ID="ID_1580256343" MODIFIED="1647139376882" POSITION="right" TEXT="mogen">
+<node CREATED="1647139377091" ID="ID_687657902" MODIFIED="1647139379317" TEXT="pengen"/>
+</node>
+<node CREATED="1647139461600" ID="ID_988562006" MODIFIED="1647139464053" POSITION="left" TEXT="sondern">
+<node CREATED="1647139464292" ID="ID_837489219" MODIFIED="1647139465470" TEXT="tapi"/>
+</node>
+</node>
+</map>
