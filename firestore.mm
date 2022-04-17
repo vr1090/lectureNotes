b8dafe7dbf11853a83572a612cb7ce575206548a
inu <vr1090@gmail.com>
2022-04-17 11:11:30 +0700

    update firestore

---
+++ firestore.mm
@@ -0,0 +1,8 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1650168522305" ID="ID_1073882677" MODIFIED="1650168530452" TEXT="firestore">
+<node CREATED="1650168531470" ID="ID_795329323" MODIFIED="1650168537109" POSITION="right" TEXT="tipe">
+<node CREATED="1650168538556" ID="ID_633615230" MODIFIED="1650168554514" TEXT="collections of documents"/>
+</node>
+</node>
+</map>
