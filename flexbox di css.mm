28c9b6eba8d1d7e5918b2f2ee0bc2b5f4ea40968
inu <vr1090@gmail.com>
2022-05-16 08:00:40 +0700

    update lecture notes

---
+++ flexbox di css.mm
@@ -0,0 +1,60 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1652661719259" ID="ID_1478427176" MODIFIED="1652661728242" TEXT="flexbox di css">
+<node CREATED="1652661955071" ID="ID_215450213" MODIFIED="1652662033595" POSITION="right" TEXT="display:flex">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      section {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;display:flex
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1652662034415" ID="ID_785329396" MODIFIED="1652662079808" POSITION="left" TEXT="axis">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      main axis
+    </p>
+    <p>
+      cross axis
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1652662523470" ID="ID_1425455495" MODIFIED="1652662528706" POSITION="right" TEXT="flex container"/>
+<node CREATED="1652662819153" ID="ID_1254483977" MODIFIED="1652662830241" POSITION="left" TEXT="flex-direction">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      default row
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
---
+++ ubuntu server.mm
@@ -0,0 +1,6 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1652567224637" ID="ID_1968351992" MODIFIED="1652567229422" TEXT="ubuntu server">
+<node CREATED="1652567230711" ID="ID_1446403505" MODIFIED="1652567233179" POSITION="right" TEXT="manage user"/>
+</node>
+</map>
