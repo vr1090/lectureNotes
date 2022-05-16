59d77dff6c235b064c21f9075c76a30e8a0c760a
inu <vr1090@gmail.com>
2022-05-16 15:49:40 +0700

    update lecture notes

--- flexbox di css.mm
+++ flexbox di css.mm
@@ -43,7 +43,7 @@
 </richcontent>
 </node>
 <node CREATED="1652662523470" ID="ID_1425455495" MODIFIED="1652662528706" POSITION="right" TEXT="flex container"/>
-<node CREATED="1652662819153" ID="ID_1254483977" MODIFIED="1652662830241" POSITION="left" TEXT="flex-direction">
+<node CREATED="1652662819153" ID="ID_1254483977" MODIFIED="1652662891195" POSITION="left" TEXT="flex-direction">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -52,9 +52,110 @@
     <p>
       default row
     </p>
+    <p>
+      column
+    </p>
+    <p>
+      row-reverse
+    </p>
+    <p>
+      column-reverse
+    </p>
   </body>
 </html>
 </richcontent>
+</node>
+<node CREATED="1652662917220" ID="ID_314499363" MODIFIED="1652662932900" POSITION="right" TEXT="wrapping">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      eventually, you will get overflow
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1652662972170" ID="ID_103369005" MODIFIED="1652662994455" POSITION="left" TEXT="flex: 200px">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      each item, will be max 200px
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1652663210503" ID="ID_1709558909" MODIFIED="1652663403527" POSITION="right" TEXT="flex-flow">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ini buat summary, flex direction sama flex-wrap
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1652663404390" ID="ID_1401401070" MODIFIED="1652663421884" POSITION="left" TEXT="flex">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      berapa banyak space, gw ambil, compare dengan item2 yg lain?
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1652690192113" ID="ID_186556595" MODIFIED="1652690197522" POSITION="right" TEXT="flex in 3 unit">
+<node CREATED="1652690197777" ID="ID_349622465" MODIFIED="1652690203056" TEXT="flex grow"/>
+<node CREATED="1652690232377" ID="ID_295589922" MODIFIED="1652690235817" TEXT="flrex-shrink"/>
+<node CREATED="1652690236066" ID="ID_1199263118" MODIFIED="1652690248438" TEXT="flex-basis"/>
+</node>
+<node CREATED="1652690323603" ID="ID_1357188764" MODIFIED="1652690399479" POSITION="left" TEXT="align-items">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      standardnya stretch
+    </p>
+    <p>
+      center
+    </p>
+    <p>
+      flex-start
+    </p>
+    <p>
+      flex-end
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1652690328410" ID="ID_744216709" MODIFIED="1652690423769" TEXT="justify-content">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ini buat main axis
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
 </node>
 </node>
 </map>
