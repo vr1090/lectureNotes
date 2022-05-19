8ec330283a37efe04258adb3e8a339d7bedb003e
inu <vr1090@gmail.com>
2022-05-19 15:51:13 +0700

    update lecture notes

--- kubernetes.mm
+++ kubernetes.mm
@@ -17,6 +17,110 @@
 </html>
 </richcontent>
 </node>
-<node CREATED="1652885128600" ID="ID_1787676997" MODIFIED="1652885133901" POSITION="left" TEXT="master slave"/>
+<node CREATED="1652885128600" ID="ID_1787676997" MODIFIED="1652948127869" POSITION="left" TEXT="master slave">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      api server
+    </p>
+    <p>
+      one entry point
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      scheduler..
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1652948127845" ID="ID_673574802" MODIFIED="1652948130596" TEXT="api server"/>
+<node CREATED="1652948130831" ID="ID_1071568532" MODIFIED="1652948252065" TEXT="scheduler">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      where to put the node
+    </p>
+    <p>
+      on which node?
+    </p>
+    <p>
+      di node ... di handle lagi sama kublet
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1652948262441" ID="ID_406675728" MODIFIED="1652948354473" TEXT="controller manager">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      detect the change state in cluster
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1652948354462" ID="ID_1325632535" MODIFIED="1652948356848" TEXT="etcd">
+<node CREATED="1652948357087" ID="ID_539754285" MODIFIED="1652948360598" TEXT="culster brain"/>
+<node CREATED="1652948361913" ID="ID_976068595" MODIFIED="1652948365006" TEXT="key value store"/>
+</node>
+</node>
+<node CREATED="1652948637733" ID="ID_878670435" MODIFIED="1652949326249" POSITION="right" TEXT="minikube">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      master sama worker kerja barengan di satu node
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      semuanya itu konek ke api server
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      minikube start --vm-driver=hyperkit
+    </p>
+    <p>
+      kubectl get nodes
+    </p>
+    <p>
+      buat dapet list of nodes
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      kubectl version .. get version kubernetes
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
 </node>
 </map>
