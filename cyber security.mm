f45a5415b89e6c4a18892ded20feb5353f1ef29d
inu <vr1090@gmail.com>
2022-05-24 23:20:01 +0700

    update lecture notes

---
+++ cyber security.mm
@@ -0,0 +1,75 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1653405916524" ID="ID_633473214" MODIFIED="1653405923365" TEXT="cyber security">
+<node CREATED="1653405923764" ID="ID_712698291" MODIFIED="1653405975212" POSITION="right" TEXT="what is">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      indentify threat, calculate the risk and handle the threat
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1653406032711" ID="ID_1821373364" MODIFIED="1653406185434" TEXT="CIA">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      confidentiality ... data dibaca sama orang yg berhak
+    </p>
+    <p>
+      integtiy ... data tidak berubah tanpa authorisasi yg benar
+    </p>
+    <p>
+      avaliability ... data bisa diakses ketika dibutuhkan
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      non repudiation ... kalau ada yg melakukan sesuatu, ada buktinya
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1653406527922" ID="ID_1080319" MODIFIED="1653406536066" TEXT="Security Operation Center"/>
+<node CREATED="1653406730479" ID="ID_671665701" MODIFIED="1653407230894" TEXT="resource">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      cisa.org
+    </p>
+    <p>
+      csrc.nist.gov
+    </p>
+    <p>
+      krebsonsecurity.com
+    </p>
+    <p>
+      threatpost.com
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
---
+++ grokking coding interview.mm
@@ -0,0 +1,10 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1653267011628" ID="ID_311345221" MODIFIED="1653267023136" TEXT="grokking coding interview">
+<node CREATED="1653267087167" ID="ID_1373203683" MODIFIED="1653267090906" POSITION="right" TEXT="sliding window">
+<node CREATED="1653267091370" ID="ID_491288842" MODIFIED="1653267094025" TEXT="math.inf"/>
+<node CREATED="1653311388626" ID="ID_1424616479" MODIFIED="1653311394936" TEXT="fruit into basked"/>
+<node CREATED="1653313043648" ID="ID_1158972945" MODIFIED="1653313049744" TEXT="non repeating sbustring.."/>
+</node>
+</node>
+</map>
