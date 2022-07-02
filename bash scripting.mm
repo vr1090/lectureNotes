1a2aa42427a0927c131045ef938e54ab4e432f37
inu <vr1090@gmail.com>
2022-07-02 09:32:56 +0700

    update lecture notes

--- bash scripting.mm
+++ bash scripting.mm
@@ -1,7 +1,7 @@
 <map version="1.1.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1656727490623" ID="ID_1728072920" MODIFIED="1656727500856" TEXT="bash scripting">
-<node CREATED="1656727722140" ID="ID_61820448" MODIFIED="1656727802693" POSITION="right" TEXT="stdin, stdout, stdErr, I/O">
+<node CREATED="1656727722140" ID="ID_61820448" MODIFIED="1656728816282" POSITION="right" TEXT="stdin, stdout, stdErr, I/O">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -18,6 +18,43 @@
     </p>
     <p>
       
+    </p>
+    <p>
+      ... we can define ... pake apa.. engga perlu cuma keyboard and layar
+    </p>
+    <p>
+      linux are file related, almost everything is file
+    </p>
+    <p>
+      stdin stdout itu file descriptor
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      proc PID, content
+    </p>
+    <p>
+      internal command ... bawaan dari bash
+    </p>
+    <p>
+      external command ... perlu load file atau library dulu
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1656729151734" ID="ID_1061757071" MODIFIED="1656729161498" POSITION="left" TEXT="variabel">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      key=value
     </p>
   </body>
 </html>
