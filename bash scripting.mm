58ba3f2e140104999da3c60d4419ae17a8c08bd2
inu <vr1090@gmail.com>
2022-07-03 17:01:57 +0700

    update lecture notes

--- bash scripting.mm
+++ bash scripting.mm
@@ -47,7 +47,7 @@
 </html>
 </richcontent>
 </node>
-<node CREATED="1656729151734" ID="ID_1061757071" MODIFIED="1656729161498" POSITION="left" TEXT="variabel">
+<node CREATED="1656729151734" ID="ID_1061757071" MODIFIED="1656729769320" POSITION="left" TEXT="variabel">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -55,6 +55,153 @@
   <body>
     <p>
       key=value
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      export key=value biar bisa kebaca sampai subshell
+    </p>
+    <p>
+      echo ${key} with currly braces
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      every command running its own subshell
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1656816299469" ID="ID_1833424309" MODIFIED="1656816846254" POSITION="right" TEXT="exit code">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      buat kasih tahu ke parent shell, hasilnya gimana
+    </p>
+    <p>
+      kalo 0 .. sucess
+    </p>
+    <p>
+      &#160;kalo 1 ... error
+    </p>
+    <p>
+      $? ... buat tanya last exit code
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1656816302437" ID="ID_1511885571" MODIFIED="1656816302437" TEXT=""/>
+</node>
+<node CREATED="1656816847547" ID="ID_901323399" MODIFIED="1656816861356" POSITION="left" TEXT="shell script">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      command for running inside the shell
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1656836516667" ID="ID_599750609" MODIFIED="1656837359386" POSITION="right" TEXT="devops">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      CI/CD, toolchains, piepline, 12 factor application development
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      toolchain: coding, building, testing , packaging, releasing, configuring, monitoring
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      idempotent .. bisa jalanin berkali kali
+    </p>
+    <p>
+      bash start corectly .. please useng shebangs ... #!/bin/bash
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      desired state .. mostly written in yaml
+    </p>
+    <p>
+      bashed action oriented ..
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1656838051645" ID="ID_1069050334" MODIFIED="1656838079416" POSITION="left" TEXT="shell pattern">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      regex, globbing
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1656840563783" ID="ID_815663403" MODIFIED="1656841945147" POSITION="right" TEXT="python open file">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      csv, open
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      pandas .. datatable ... a dataframe, mirip kayak spreadsheet
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      common log format, CLF.
+    </p>
+    <p>
+      cryptography.Fernet
+    </p>
+    <p>
+      pathlib
+    </p>
+    <p>
+      os.walk
     </p>
   </body>
 </html>
---
+++ monitoring gcp.mm
@@ -0,0 +1,40 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1655845836942" ID="ID_1338123081" MODIFIED="1655845845645" TEXT="monitoring gcp">
+<node CREATED="1655845846139" ID="ID_617416029" MODIFIED="1655845888867" POSITION="right" TEXT="path">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      capture signal .. visualize ... manage incident
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1655846069714" ID="ID_1141624437" MODIFIED="1655904241534" POSITION="left" TEXT="operation">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      monitoring, logging, service monitoring,
+    </p>
+    <p>
+      signal .. open telemetry ...
+    </p>
+    <p>
+      logs .. audit log, agent log, network logs
+    </p>
+    <p>
+      fluentd
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
