08743f82cfaf92769ad4f0a0e4a5978b93f859d5
inu <vr1090@gmail.com>
2022-11-27 20:00:45 +0700

    update lecture notes

---
+++ leetcode.mm
@@ -0,0 +1,15 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1669553936268" ID="ID_296581431" MODIFIED="1669553953908" TEXT="leetcode">
+<node CREATED="1669553954290" ID="ID_1976426771" MODIFIED="1669553958526" POSITION="right" TEXT="level 1">
+<node CREATED="1669553958760" ID="ID_628291919" MODIFIED="1669553961848" TEXT="day 8">
+<node CREATED="1669553962083" ID="ID_425959522" MODIFIED="1669553984635" TEXT="validate">
+<node CREATED="1669553984972" ID="ID_1142311650" MODIFIED="1669553993930" TEXT="kasih max dan min"/>
+<node CREATED="1669553994694" ID="ID_759447579" MODIFIED="1669554001962" TEXT="make sure semua ikutin">
+<node CREATED="1669554002586" ID="ID_668507705" MODIFIED="1669554006562" TEXT="jump the faith"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
