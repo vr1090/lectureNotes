45fd7b2455f1bc472cbf5ccca70e0352e61dd933
inu <vr1090@gmail.com>
2023-01-14 21:56:55 +0700

    update lecture notes

---
+++ aws.mm
@@ -0,0 +1,8 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1673708168963" ID="ID_1994154447" MODIFIED="1673708175403" TEXT="aws">
+<node CREATED="1673708175848" ID="ID_1475329947" MODIFIED="1673708178865" POSITION="right" TEXT="monitoring">
+<node CREATED="1673708179114" ID="ID_1405897692" MODIFIED="1673708184655" TEXT="cloud watch metric"/>
+</node>
+</node>
+</map>
