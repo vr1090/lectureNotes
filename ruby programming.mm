fcaa68cd92f41aa2f13e7b355673fc1f649e5c2f
inu <vr1090@gmail.com>
2023-01-14 19:06:37 +0700

    update lecture notes

---
+++ ruby programming.mm
@@ -0,0 +1,4 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1673697951931" ID="ID_528883690" MODIFIED="1673697959480" TEXT="ruby programming"/>
+</map>
