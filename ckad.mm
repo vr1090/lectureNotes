605c7b0f53a7a2cf7c95022964effd17ce09bf24
inu <vr1090@gmail.com>
2023-03-13 21:44:56 +0700

    update lecture notes

---
+++ ckad.mm
@@ -0,0 +1,12 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1678715872327" ID="ID_680925612" MODIFIED="1678715877046" TEXT="ckad">
+<node CREATED="1678715916853" ID="ID_1999191185" MODIFIED="1678715942286" POSITION="right" TEXT="multi pod">
+<node CREATED="1678715922324" ID="ID_188062672" MODIFIED="1678715946401" TEXT="sidecar"/>
+<node CREATED="1678715946649" ID="ID_1195057989" MODIFIED="1678715949334" TEXT="adaptor"/>
+<node CREATED="1678715949584" ID="ID_807013597" MODIFIED="1678715952315" TEXT="ambassaodr"/>
+</node>
+<node CREATED="1678716129346" ID="ID_791321054" MODIFIED="1678716135695" POSITION="left" TEXT="pos uses service account"/>
+<node CREATED="1678716571956" ID="ID_190220819" MODIFIED="1678716592633" POSITION="right" TEXT="kubectl config set-context namacontext --namespace=nama_name-space"/>
+</node>
+</map>
