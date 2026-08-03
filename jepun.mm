5a3bc6cad00549e813f96ed19f62bb321bbe4677
v.adhi <vr1090@gmail.com>
2026-08-03 07:34:47 +0700

    update jepun map

---
+++ jepun.mm
@@ -0,0 +1,121 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="jepung" FOLDED="false" ID="ID_1247506417" CREATED="1774188960450" MODIFIED="1774188964888" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" background="#cccccc">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="raisu" POSITION="right" ID="ID_1561586012" CREATED="1774188965065" MODIFIED="1774188968062">
+<edge COLOR="#ff0000"/>
+<node TEXT="next week" ID="ID_1302885760" CREATED="1774188968250" MODIFIED="1774188972662"/>
+</node>
+<node TEXT="kaimasu" POSITION="left" ID="ID_154876005" CREATED="1774189033851" MODIFIED="1774189036014">
+<edge COLOR="#0000ff"/>
+<node TEXT="buying" ID="ID_353461784" CREATED="1774189036190" MODIFIED="1774189038743"/>
+</node>
+<node TEXT="aso.bi.masu" POSITION="right" ID="ID_1014210718" CREATED="1774189421441" MODIFIED="1774189426865">
+<edge COLOR="#00ff00"/>
+<node TEXT="hangout" ID="ID_1521075108" CREATED="1774189427045" MODIFIED="1774189430122"/>
+</node>
+<node TEXT="mai-syu" POSITION="left" ID="ID_537220019" CREATED="1774390621190" MODIFIED="1774390625985">
+<edge COLOR="#ff00ff"/>
+<node TEXT="mai itu every" ID="ID_1838602679" CREATED="1774390626185" MODIFIED="1774390633301"/>
+<node TEXT="syu" ID="ID_1445357378" CREATED="1774390633508" MODIFIED="1774390637435">
+<node TEXT="minggu" ID="ID_1020919324" CREATED="1774390637610" MODIFIED="1774390640307"/>
+</node>
+</node>
+<node TEXT="de.ka.ke.masu" POSITION="right" ID="ID_735812131" CREATED="1774390676140" MODIFIED="1774390680994">
+<edge COLOR="#00ffff"/>
+<node TEXT="go out" ID="ID_936124880" CREATED="1774390681187" MODIFIED="1774390694681"/>
+</node>
+<node TEXT="yasui" POSITION="left" ID="ID_441511355" CREATED="1785679457080" MODIFIED="1785679460190">
+<edge COLOR="#7c0000"/>
+<node TEXT="murah" ID="ID_1672351246" CREATED="1785679460414" MODIFIED="1785679464638"/>
+</node>
+<node TEXT="tsukue" POSITION="left" ID="ID_903023978" CREATED="1785679608786" MODIFIED="1785679611056">
+<edge COLOR="#00007c"/>
+<node TEXT="desk" ID="ID_1930164578" CREATED="1785679611254" MODIFIED="1785679613083"/>
+</node>
+<node TEXT="midori" POSITION="right" ID="ID_1269883571" CREATED="1785679643737" MODIFIED="1785679650143">
+<edge COLOR="#007c00"/>
+<node TEXT="green" ID="ID_1537638373" CREATED="1785679650682" MODIFIED="1785679652616"/>
+</node>
+<node TEXT="family relationship japan" POSITION="right" ID="ID_612832580" CREATED="1785716984316" MODIFIED="1785716992673">
+<edge COLOR="#7c007c"/>
+<node TEXT="kyodai" ID="ID_111589453" CREATED="1785717037325" MODIFIED="1785717039774">
+<node TEXT="adek kakak" ID="ID_380747975" CREATED="1785717039955" MODIFIED="1785717048395"/>
+</node>
+<node TEXT="adik" ID="ID_1385711840" CREATED="1785717195798" MODIFIED="1785717197961">
+<node TEXT="ototo" ID="ID_114906538" CREATED="1785717198197" MODIFIED="1785717202335"/>
+<node TEXT="ototosan" ID="ID_1125927576" CREATED="1785717202781" MODIFIED="1785717208866"/>
+</node>
+<node TEXT="kakak laki2" ID="ID_1995305075" CREATED="1785717209869" MODIFIED="1785717214772">
+<node TEXT="ani" ID="ID_1706347046" CREATED="1785717215040" MODIFIED="1785717222025"/>
+<node TEXT="onisan" ID="ID_1161082206" CREATED="1785717222165" MODIFIED="1785717225861"/>
+</node>
+<node TEXT="kakak perempuan" ID="ID_628572112" CREATED="1785717226698" MODIFIED="1785717231251">
+<node TEXT="ane" ID="ID_1928926232" CREATED="1785717231440" MODIFIED="1785717233909"/>
+<node TEXT="onessan" ID="ID_887989333" CREATED="1785717234269" MODIFIED="1785717237855"/>
+</node>
+</node>
+</node>
+</map>
